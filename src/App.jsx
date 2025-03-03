0a043c3e11da13be63cda321e16d2832262a9045
AliH2001 <ali.hussain.jaffer.2001@gmail.com>
2025-03-03 12:51:48 +0300

    edit navbar and create recipeform

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Landing from './components/Landing/Landing';
 import Dashboard from './components/Dashboard/Dashboard';
 import SignupForm from './components/SignupForm/SignupForm';
 import SigninForm from './components/SigninForm/SigninForm';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import * as authService from '../src/services/authService'; // import the authservice
 
 export const AuthedUserContext = createContext(null);
