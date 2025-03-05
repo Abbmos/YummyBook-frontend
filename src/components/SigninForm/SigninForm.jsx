592a03fbae08a9905ca3d621f285ad35f39c1dc5
AliH2001 <ali.hussain.jaffer.2001@gmail.com>
2025-03-05 12:17:42 +0300

    edit singup, singin, navbar and landing , create SignupForm.module.css and Landing.module.css

--- src/components/SigninForm/SigninForm.jsx
+++ src/components/SigninForm/SigninForm.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import * as authService from '../../services/authService';
+import styles from './SigninForm.module.css';
 
 const SigninForm = (props) => {
   const navigate = useNavigate();
-  const [message, setMessage] = useState(['']);
+  const [message, setMessage] = useState('');
   const [formData, setFormData] = useState({
     username: '',
     password: '',
@@ -32,39 +33,47 @@ const SigninForm = (props) => {
   };
 
   return (
-    <main>
-      <h1>Log In</h1>
-      <p>{message}</p>
-      <form autoComplete="off" onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="email">Username:</label>
-          <input
-            type="text"
-            autoComplete="off"
-            id="username"
-            value={formData.username}
-            name="username"
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            autoComplete="off"
-            id="password"
-            value={formData.password}
-            name="password"
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <button>Log In</button>
-          <Link to="/">
-            <button>Cancel</button>
-          </Link>
-        </div>
-      </form>
+    <main className={styles.container}>
+     
+      <div className={styles.leftPanel}>
+        <h2>Welcome Back</h2>
+        <Link to="/signup">
+          <button className={styles.createAccountBtn}>Create Account</button>
+        </Link>
+      </div>
+
+      
+      <div className={styles.rightPanel}>
+        <h1 className={styles.signinTitle}>Sign In!</h1>
+        <p className={styles.errorMessage}>{message}</p>
+        <form autoComplete="off" onSubmit={handleSubmit}>
+          <div className={styles.inputGroup}>
+            <label htmlFor="username">Email:</label>
+            <input
+              type="text"
+              autoComplete="off"
+              id="username"
+              value={formData.username}
+              name="username"
+              onChange={handleChange}
+            />
+          </div>
+          <div className={styles.inputGroup}>
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              autoComplete="off"
+              id="password"
+              value={formData.password}
+              name="password"
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <button className={styles.signinBtn}>Sign In!</button>
+          </div>
+        </form>
+      </div>
     </main>
   );
 };
